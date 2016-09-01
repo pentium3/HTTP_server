64396793100c44935aa794fb27606eb7e2da1dc8
wyl <i@hxdl.org>
2016-09-01 22:52:56 +0800

    2nd

---
+++ tiny/read.py
@@ -0,0 +1,26 @@
+
+import urllib2,threading
+cnt=0
+
+class MyThread(threading.Thread):
+	def __init__(self,threadname):
+		threading.Thread.__init__(self,name=threadname)
+
+	def run(self):
+		print(self.getName())
+		global cnt
+		cnt+=1
+		print("thread.no",cnt)
+		while(1):
+			for retry in range(10000):
+				try:
+					urllib2.urlopen("http://192.168.2.107:1033/home.html")
+				except:
+					print("TimeOut Error")
+					pass
+
+for i in range(0,1000):
+	my = MyThread('test')
+	#my.setDaemon(True)
+	my.start()
+
